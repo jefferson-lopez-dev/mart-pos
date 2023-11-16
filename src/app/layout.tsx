952c13cdb6db3a9249523923b2bab4572aea6416
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-15 19:59:58 -0500

    import Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { MartPosProvider } from "@/context";
 import "@/sass/index.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <MartPosProvider>{children}</MartPosProvider>
+      </body>
     </html>
   );
 }
