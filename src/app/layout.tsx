8d325e70c31eaef5a2f1317ea0f8e925e4c01358
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-17 09:12:13 -0500

    deleted favicon of head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,16 @@
 import type { Metadata } from "next";
 import { MartPosProvider } from "@/context";
 import "@/sass/index.css";
+import { children } from "@/interface";
 
 export const metadata: Metadata = {
   title: "Mart POS",
   description: "Mart POS",
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: children) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
       <body>
         <MartPosProvider>{children}</MartPosProvider>
       </body>
