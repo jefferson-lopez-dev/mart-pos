35823f54cedb72f239f264f8a9e8570883733197
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-17 19:36:26 -0500

    create interface

--- src/hooks/use-profile/index.ts
+++ src/hooks/use-profile/index.ts
@@ -1,8 +1,17 @@
 import { ProfileUserContext } from "@/context";
 import { useContext } from "react";
 
-export const useProfileUser = () => {
-  const context = useContext(ProfileUserContext);
+interface useProfileProps {
+  updateProfile: (data: object) => Promise<any>;
+  changePhoto: () => Promise<any>;
+  deletePhoto: () => Promise<any>;
+  getProfile: () => Promise<any>;
+  profilePhoto: any;
+  data: any;
+}
+
+export const useProfile = () => {
+  const context = useContext(ProfileUserContext) as useProfileProps;
   if (!context)
     throw new Error("useProfileUser must be used within a ProfileUserContext");
   return context;
