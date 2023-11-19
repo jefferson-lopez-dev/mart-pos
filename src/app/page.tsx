678acffda38b39bc5c8129b04e52c36d9a3b2e09
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-18 19:46:45 -0500

    ignore taildwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import { DataAccount } from "../components/DataAccount";
 export default function Home() {
   return (
     <div>
-      <button>asdas</button>
       <DataAccount></DataAccount>
     </div>
   );
