f0b6478f99a5b81b8da4658e5e87404d2fe8984b
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-23 17:02:02 -0500

    deleted rutes

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -40,14 +40,14 @@ export default function AuthenticationPage() {
               <UserAuthForm />
               <p className="px-8 text-center text-sm text-muted-foreground">
                 <Link
-                  href="/terms"
+                  href="/"
                   className="underline underline-offset-4 hover:text-primary"
                 >
                   Terms of Service
                 </Link>{" "}
                 and{" "}
                 <Link
-                  href="/privacy"
+                  href="/"
                   className="underline underline-offset-4 hover:text-primary"
                 >
                   Privacy Policy
