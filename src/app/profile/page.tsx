8a86d19b4ff57811c294b3194e284c3c81863592
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-25 17:38:16 -0500

    view sessions

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,13 @@
+"use client";
 import { DataProfile } from "@/components/DataProfile";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
 
-export default function profile() {
+export default function Profile() {
+  const { data: session, status } = useSession();
+
+  console.log(session);
+
   return (
     <div>
       <Link href={"/"}>back</Link>
