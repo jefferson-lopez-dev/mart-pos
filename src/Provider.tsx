fca68253462ffed2908276deb2fffffcf3cb8ee5
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-11-25 17:35:41 -0500

    provider next auth

--- src/Provider.tsx
+++ src/Provider.tsx
@@ -3,26 +3,27 @@ import { AuthProvider, ProfileUserProvider } from "./context";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { children } from "./interface";
 import { Header } from "./components/organisms/Header";
-import { usePathname, useSearchParams } from "next/navigation";
-import Error from "next/error";
+import { usePathname } from "next/navigation";
+import { SessionProvider } from "next-auth/react";
 
 export function MartProvider({ children }: children) {
   const path = usePathname();
-
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <AuthProvider>
-        <ProfileUserProvider>
-          {path === "/" && <Header />}
-          {path === "/profile" && <Header />}
-          {children}
-        </ProfileUserProvider>
-      </AuthProvider>
-    </ThemeProvider>
+    <SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <AuthProvider>
+          <ProfileUserProvider>
+            {path === "/" && <Header />}
+            {path === "/profile" && <Header />}
+            {children}
+          </ProfileUserProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </SessionProvider>
   );
 }
