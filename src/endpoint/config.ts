684b2ce56eece0f46758447decc7388f08d9209f
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-12-02 21:18:07 -0500

    change url

--- src/endpoint/config.ts
+++ src/endpoint/config.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 //API_PRODUCTION_URL
 
 export const profile = axios.create({
-  baseURL: "http://localhost:3014/api/creds_profile",
+  baseURL: "https://api-mart-profile.onrender.com/api/creds_profile",
   withCredentials: true,
 });
