5ee86f2458c7b46c0d52b73b47a0447ec4447bb0
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-12-20 18:08:46 -0500

    create vars url pos

--- src/endpoint/config.ts
+++ src/endpoint/config.ts
@@ -1,10 +1,19 @@
 import axios from "axios";
 
-const endPoint = "api/creds_profile";
-const API_PRODUCTION_URL = `https://api-mart-profile.onrender.com/${endPoint}`;
-const API_LOCAL_URL = ` http://localhost:3014/${endPoint}`;
+const ENDPOINT_PROFILE = "api/creds_profile";
+const API_PRODUCTION_PROFILE_URL = `https://api-mart-profile.onrender.com/${ENDPOINT_PROFILE}`;
+const API_LOCAL_PROFILE_URL = ` http://localhost:3014/${ENDPOINT_PROFILE}`;
+
+const ENDPOINT_POS = "api/pos";
+const API_PRODUCTION_POS_URL = `http://localhost:1204/${ENDPOINT_POS}`;
+const API_LOCAL_POS_URL = `http://localhost:1204/${ENDPOINT_POS}`;
 
 export const profile = axios.create({
-  baseURL: API_PRODUCTION_URL,
+  baseURL: API_PRODUCTION_PROFILE_URL,
+  withCredentials: true,
+});
+
+export const pos = axios.create({
+  baseURL: API_LOCAL_POS_URL,
   withCredentials: true,
 });
