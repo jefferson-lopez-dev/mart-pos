acfce738e0be9b1a445e82a1507d2daaa3b92db1
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-12-26 12:15:44 -0500

    add scroll area and new buttons

--- src/components/header-page/main-nav.tsx
+++ src/components/header-page/main-nav.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 import { Button } from "../ui/button";
+import { ScrollArea, ScrollBar } from "../ui/scroll-area";
 
 export function MainNav({
   className,
@@ -10,52 +11,66 @@ export function MainNav({
   const pn = usePathname();
 
   return (
-    <nav
-      className={cn("flex items-center space-x-4 lg:space-x-6", className)}
-      {...props}
-    >
-      <Button size="sm" asChild>
+    <ScrollArea className="w-full">
+      <nav className={cn("flex items-center space-x-6", className)} {...props}>
+        <Link
+          href="/"
+          className={`break-normal whitespace-nowrap text-sm font-medium ${
+            pn !== "/" && "text-muted-foreground"
+          } transition-colors hover:text-primary`}
+        >
+          Check In
+        </Link>
         <Link
           href="/"
           className={`break-normal text-sm font-medium ${
-            pn !== "/" && "text-muted-foreground"
-          } transition-colors`}
+            pn !== "/sales" && "text-muted-foreground"
+          } transition-colors hover:text-primary`}
         >
-          Check in
+          Dashboard
         </Link>
-      </Button>
-      <Link
-        href="/"
-        className={`break-normal text-sm font-medium ${
-          pn !== "/Overview" && "text-muted-foreground"
-        } transition-colors hover:text-primary`}
-      >
-        Overview
-      </Link>
-      <Link
-        href="/"
-        className={`break-normal text-sm font-medium ${
-          pn !== "/sales" && "text-muted-foreground"
-        } transition-colors hover:text-primary`}
-      >
-        Sales
-      </Link>
-      <Link
-        href="/"
-        className={`break-normal text-sm font-medium ${
-          pn !== "/inventory" && "text-muted-foreground"
-        } transition-colors hover:text-primary`}
-      >
-        Inventory
-      </Link>
-      <Link
-        href="/settings"
-        className={`break-normal text-sm font-medium ${
-          pn !== "/settings" && "text-muted-foreground"
-        } transition-colors hover:text-primary`}
-      >
-        Settings
-      </Link>
-    </nav>
+        <Link
+          href="/"
+          className={`break-normal text-sm font-medium ${
+            pn !== "/sales" && "text-muted-foreground"
+          } transition-colors hover:text-primary`}
+        >
+          Sales
+        </Link>
+        <Link
+          href="/"
+          className={`break-normal text-sm font-medium ${
+            pn !== "/inventory" && "text-muted-foreground"
+          } transition-colors hover:text-primary`}
+        >
+          Inventories
+        </Link>
+        <Link
+          href="/"
+          className={`break-normal text-sm font-medium ${
+            pn !== "/Overview" && "text-muted-foreground"
+          } transition-colors hover:text-primary`}
+        >
+          Folders
+        </Link>
+        <Link
+          href="/"
+          className={`break-normal text-sm font-medium ${
+            pn !== "/Overview" && "text-muted-foreground"
+          } transition-colors hover:text-primary`}
+        >
+          Products
+        </Link>
+        <Link
+          href="/settings"
+          className={`break-normal text-sm font-medium ${
+            pn !== "/settings" && "text-muted-foreground"
+          } transition-colors hover:text-primary`}
+        >
+          Settings
+        </Link>
+      </nav>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
   );
 }
