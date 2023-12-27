a7da7b2233d93fe23024c197e54c52c76d36fd1a
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2023-12-26 21:43:36 -0500

    create footer page

---
+++ src/components/footer-page/index.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Laptop2, Moon, Sun } from "lucide-react";
+import { Button } from "../ui/button";
+
+export function FooterPage() {
+  const { theme, systemTheme, setTheme } = useTheme();
+  const { push } = useRouter();
+  const [logo, setLogo] = useState("/mart-light.png");
+
+  useEffect(() => {
+    function logoMart() {
+      if (theme === "dark" || systemTheme === "dark")
+        setLogo("/mart-light.png");
+
+      if (theme === "light" || systemTheme === "light")
+        setLogo("/mart-dark.png");
+    }
+    logoMart();
+  }, [theme, systemTheme]);
+  return (
+    <footer className="flex w-full justify-center h-[100px] items-center border-t">
+      <div className="flex w-full max-w-[730px] justify-between">
+        <div className="flex items-center pl-3">
+          <Image
+            className="h-[30px] w-[30px] cursor-pointer"
+            onClick={() => push("/")}
+            src={logo}
+            alt="photo"
+            width={35}
+            height={35}
+          />
+          <p className="text-mart text-[30px]">MART</p>
+        </div>
+        <div className="flex items-center pr-3">
+          <Button
+            onClick={() => setTheme("light")}
+            variant={theme === "light" ? "secondary" : "ghost"}
+            className="rounded-full p-[10px]"
+          >
+            <Sun className="icon2" />
+          </Button>
+          <Button
+            onClick={() => setTheme("dark")}
+            variant={theme === "dark" ? "secondary" : "ghost"}
+            className="rounded-full p-[10px]"
+          >
+            <Moon className="icon2" />
+          </Button>
+          <Button
+            onClick={() => setTheme("system")}
+            variant={theme === "system" ? "secondary" : "ghost"}
+            className="rounded-full p-[10px]"
+          >
+            <Laptop2 className="icon2" />
+          </Button>
+        </div>
+      </div>
+    </footer>
+  );
+}
