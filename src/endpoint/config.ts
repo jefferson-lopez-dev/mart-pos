222b9ea8d24850367d6b366c372ea74932bf6f3d
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-01-01 13:58:04 -0500

    use api pos production

--- src/endpoint/config.ts
+++ src/endpoint/config.ts
@@ -5,7 +5,7 @@ const API_PRODUCTION_PROFILE_URL = `https://api-mart-profile.onrender.com/${ENDP
 const API_LOCAL_PROFILE_URL = ` http://localhost:3014/${ENDPOINT_PROFILE}`;
 
 const ENDPOINT_POS = "api/pos";
-const API_PRODUCTION_POS_URL = `http://localhost:1204/${ENDPOINT_POS}`;
+const API_PRODUCTION_POS_URL = `https://api-mart-pos.onrender.com/${ENDPOINT_POS}`;
 const API_LOCAL_POS_URL = `http://localhost:1204/${ENDPOINT_POS}`;
 
 export const profile = axios.create({
@@ -14,5 +14,5 @@ export const profile = axios.create({
 });
 
 export const pos = axios.create({
-  baseURL: API_LOCAL_POS_URL,
+  baseURL: API_PRODUCTION_POS_URL,
 });
