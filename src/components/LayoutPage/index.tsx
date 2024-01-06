ed5037a665a6943e2dc8cc7c4aefc7aacfa612a8
Jefferson Lopez <jeffersonlopezmendoza343@gmail.com>
2024-01-06 12:11:09 -0500

    solucione bug

--- src/components/LayoutPage/index.tsx
+++ src/components/LayoutPage/index.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 export function LayoutPage({ children }: children) {
   return (
-    <div className="w-full flex justify-center">
-      <div className="w-full max-w-[730px] flex justify-center">{children}</div>
+    <div className="w-full flex justify-center pb-5">
+      <div className="w-full max-w-[730px] flex justify-center flex-col">
+        {children}
+      </div>
     </div>
   );
 }
